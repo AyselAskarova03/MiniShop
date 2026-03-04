8779d2b222c46380df4f6aa877ba15da64f10737
AyselAskarova03 <ayselaskrva03@gmail.com>
2026-03-04 20:51:46 +0400

    updated  project

--- src/admin/components/AdminSidebar.tsx
+++ src/admin/components/AdminSidebar.tsx
@@ -1,22 +1,23 @@
 import { NavLink } from "react-router-dom";
+import styles from "./AdminSidebar.module.css";
 
 export default function AdminSidebar() {
   return (
-    <div
-      style={{
-        width: "220px",
-        background: "#111",
-        color: "white",
-        minHeight: "100vh",
-        padding: "20px",
-      }}
-    >
-      <h2>ADMIN</h2>
+    <div className={styles.sidebar}>
+      <h2 className={styles.title}>ADMIN</h2>
 
-      <nav style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
-        <NavLink to="/admin">Dashboard</NavLink>
-        <NavLink to="/admin/products">Products</NavLink>
-        <NavLink to="/admin/add-product">Add Product</NavLink>
+      <nav className={styles.nav}>
+        <NavLink to="/admin" className={styles.link}>
+          Dashboard
+        </NavLink>
+
+        <NavLink to="/admin/products" className={styles.link}>
+          Products
+        </NavLink>
+
+        <NavLink to="/admin/add-product" className={styles.link}>
+          Add Product
+        </NavLink>
       </nav>
     </div>
   );
